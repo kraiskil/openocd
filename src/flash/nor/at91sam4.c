9f069e0f7432f27989d2390cdce5bae3da5910f6
Kalle Raiskila <kalle.raiskila@nokia.com>
2017-03-26 16:41:48 +0300

    ID for at91samg55j19

--- src/flash/nor/at91sam4.c
+++ src/flash/nor/at91sam4.c
@@ -1340,6 +1340,42 @@ static const struct sam4_chip_details all_sam4_details[] = {
 		}
 	},
 
+	/* at91samg55j19 */
+	{
+		.chipid_cidr    = 0x24570ae0,
+		.name           = "at91samg55j19",
+		.total_flash_size     = 512 * 1024,
+		.total_sram_size      = 160 * 1024,
+		.n_gpnvms       = 2,
+		.n_banks        = 1,
+
+/*		.bank[0] = {*/
+		{
+			{
+				.probed = 0,
+				.pChip  = NULL,
+				.pBank  = NULL,
+				.bank_number = 0,
+				.base_address = FLASH_BANK_BASE_S,
+				.controller_address = 0x400e0a00,
+				.flash_wait_states = 6,	/* workaround silicon bug */
+				.present = 1,
+				.size_bytes =  512 * 1024,
+				.nsectors   =  64,
+				.sector_size = 8192,
+				.page_size   = 512,
+			},
+/*		.bank[1] = {*/
+		  {
+			.present = 0,
+			.probed = 0,
+			.bank_number = 1,
+
+		  },
+		}
+	},
+
+
 	/* terminate */
 	{
 		.chipid_cidr    = 0,
